650494c4b4a60fdb803b4f8de89ff5292894d161
suri <swigder@gmail.com>
2018-08-05 18:16:06 -0400

    plot number of people in town

--- turnstiles.py
+++ turnstiles.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+sns.set()
 
 # read csv, including parsing dates
 # todo all files
@@ -22,9 +26,34 @@ df[['ENTRIES_P', 'EXITS_P']] = df[['ENTRIES_P', 'EXITS_P']].clip(lower=0, upper=
 station_totals = df.groupby(['STATION', 'LINENAME'])['ENTRIES_P', 'EXITS_P'].sum()
 
 
+station = '77 ST'
+line = 'R'
+
+
+def generate_station_series(station, line):
+    return df[(df.STATION == station) & (df.LINENAME == line)].groupby('DATE_TIME').sum().reset_index()
+
+
 def plot_station_over_time(station, line):
-    station_series = df[(df.STATION == station) & (df.LINENAME == line)].groupby('DATE_TIME').sum().reset_index()
+    station_series = generate_station_series(station, line)
     plt.plot(station_series.DATE_TIME, station_series.ENTRIES_P)
     plt.plot(station_series.DATE_TIME, station_series.EXITS_P)
+    plt.legend()
+    plt.show()
+
+
+def plot_people_in_town(station, line):
+    station_series = generate_station_series(station, line)
+
+    # some stations have exit-only turnstiles that don't record data.
+    # to account for this, make the assumption that over a week, there are the same number
+    # of entries and exits per station, and adjust accordingly
+    proportion_entries_exits = station_series.sum().ENTRIES_P / station_series.EXITS_P.sum()
+    station_series.EXITS_P *= proportion_entries_exits
+
+    # number of people in town is the difference between # that enter town (exit station) and exit town (enter station)
+    station_series['IN_TOWN'] = station_series.EXITS_P - station_series.ENTRIES_P
+    plt.plot(station_series.DATE_TIME, station_series.IN_TOWN.cumsum())
+    plt.xticks(rotation=90)
     plt.legend()
     plt.show()
